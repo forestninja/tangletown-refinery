b1f0c44bb8121c99081fb281b32442c82a5c567d
Georgia King <georgia@forestninja.net>
2014-11-05 01:35:26 +0000

    installed refinery

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TangletownRefinery::Application.config.secret_token = '0c33f050c3f4c003d0e36a5d167281ce0899b89036462f00a10eb8a59ab69c846451856d8e93253e06661aca3534a3037ab5d8e3cb72b02c5d82d168a8f84958'
